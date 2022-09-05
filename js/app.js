1613afe3c9acbb8fc34447cc54246e9cf30f9785
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-05 12:50:33 -0300

    136 - Leer el valor de la marca del automovil

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 // Variables
 const resultado = document.querySelector('#resultado');
 
-const selectYear = document.querySelector('#year')
+const selectMarca = document.querySelector('#marca');
+const selectYear = document.querySelector('#year');
+const selectMinimo = document.querySelector('#minimo');
+const selectMaximo = document.querySelector('#maximo');
+const selectPuertas = document.querySelector('#puertas');
+const selectTransmision = document.querySelector('#transmision');
+const selectColor = document.querySelector('#color');
+
 const fechaActual = new Date().getFullYear(); // accedo al año actual
 const fechaMinima = fechaActual - 10; // el año minimo de los modelos que voy a vender
+
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,8 +30,30 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelectYears();
 })
 
-// Funciones
+selectMarca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+selectYear.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+selectMinimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+selectMaximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+selectPuertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+selectTransmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+selectColor.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+})
 
+
+// Funciones
 // Creo el HTML de todos los autos existentes.
 function mostrarAutos () {
     autos.forEach( elemento => {
